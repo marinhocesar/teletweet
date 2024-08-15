18d54a63c1709e888018043fd2686d86980077ad
marinhocesar <marinhocaiocesar@gmail.com>
2024-08-14 22:20:27 -0300

    feat: convert to run without webhook

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM python:3.11-buster
 
-WORKDIR /app
-COPY ./requirements.txt /app/requirements.txt
-COPY . /app
-RUN python3 -m pip install -r /app/requirements.txt
+RUN pip install poetry
 
-EXPOSE 5000
-CMD ["gunicorn", "-b", "0.0.0.0:5000", "app:app"]
+COPY . .
+
+RUN poetry install
+
+ENTRYPOINT ["poetry", "run", "python", "app.py"]
--- src/telegram_handler.py
+++ src/telegram_handler.py
@@ -1,7 +1,13 @@
 import logging
 import os
 from telegram import Update
-import telegram
+from telegram.ext import (
+    CommandHandler,
+    MessageHandler,
+    filters,
+    CallbackContext,
+    ApplicationBuilder,
+)
 
 from config import (
     TELEGRAM_TOKEN,
@@ -19,73 +25,81 @@ class TelegramBot:
         logging.warning("----------------------------------")
 
         self.twitter = TwitterHandler()
-        self.bot = telegram.Bot(token=TELEGRAM_TOKEN)
+        try:
+            self.application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+        except Exception:
+            logging.error("could not build telegram connection.")
+            raise ConnectionError
 
-    async def start(self, update: Update):
-        if update.message is None:
-            return
-
-        await self.bot.send_message(
-            chat_id=update.message.chat_id,
-            text="Hi! Send me a message or photo, and I will tweet it.",
+        handled_message_types = filters.TEXT & filters.PHOTO
+        self.application.add_handler(CommandHandler("start", start))
+        self.application.add_handler(
+            MessageHandler(filters.TEXT & ~filters.COMMAND, tweet_text)
+        )
+        self.application.add_handler(MessageHandler(filters.PHOTO, tweet_photo))
+        self.application.add_handler(
+            MessageHandler(~handled_message_types, message_handler_not_implemented)
         )
 
-    async def tweet_text(self, update: Update):
-        if update.message is None:
-            return
 
-        message_text = update.message.text
-        try:
-            twitter = TwitterHandler()
-            twitter.tweet_text(text=message_text or "")
-            logging.warning("tweet sent")
-            await self.bot.send_message(
-                chat_id=update.message.chat_id, text="Tweeted: " + (message_text or "")
-            )
-        except Exception as e:
-            logging.error("could not send tweet")
-            await self.bot.send_message(
-                chat_id=update.message.chat_id, text="Failed to tweet. Error: " + str(e)
-            )
-
-    async def tweet_photo(self, update: Update):
-        if update.message is None:
-            return
-
-        message_text = update.message.caption
-        photo = update.message.photo[-1]
-        photo.set_bot(bot=self.bot)
-        photo_file = await photo.get_file()  # Await the coroutine to get the file object
-        photo_path = os.path.join("/tmp", f"{update.message.photo[-1].file_id}.jpg")
-
-        twitter = TwitterHandler()
-        await photo_file.download_to_drive(
-            photo_path
-        )  # Await the coroutine to download the file
-        media = twitter.simple_upload(filename=photo_path)
-        media_id = media.media_id
-        try:
-            twitter.tweet_photo(text=message_text or "", media_id=media_id)
-            logging.warning("tweet sent")
-            await self.bot.send_message(
-                chat_id=update.message.chat_id, text="Tweeted photo!"
-            )
-        except Exception as e:
-            logging.error("could not send tweet")
-            await self.bot.send_message(
-                chat_id=update.message.chat_id,
-                text="Failed to tweet photo. Error: " + str(e),
-            )
-        finally:
-            os.remove(photo_path)
-
-    async def message_handler_not_implemented(self, update: Update):
-        if update.message is None:
-            return
-
-        logging.error("type of message not supported")
-        await self.bot.send_message(
-            chat_id=update.message.chat_id,
-            text="This type of message is not yet supported in the context of this application",
-        )
+async def start(update: Update, context: CallbackContext):
+    if update.message is None:
         return
+
+    await update.message.reply_text(
+        text="Hi! Send me a message or photo, and I will tweet it.",
+    )
+
+
+async def tweet_text(update: Update, context: CallbackContext):
+    if update.message is None:
+        return
+
+    message_text = update.message.text
+    try:
+        twitter = TwitterHandler()
+        twitter.tweet_text(text=message_text or "")
+        logging.warning("tweet sent")
+        await update.message.reply_text(text="Tweeted: " + (message_text or ""))
+    except Exception as e:
+        logging.error("could not send tweet")
+        await update.message.reply_text(text="Failed to tweet. Error: " + str(e))
+
+
+async def tweet_photo(update: Update, context: CallbackContext):
+    if update.message is None:
+        return
+
+    message_text = update.message.caption
+    photo = update.message.photo[-1]
+    photo_file = await photo.get_file()  # Await the coroutine to get the file object
+    photo_path = os.path.join("/tmp", f"{update.message.photo[-1].file_id}.jpg")
+
+    twitter = TwitterHandler()
+    await photo_file.download_to_drive(
+        photo_path
+    )  # Await the coroutine to download the file
+    media = twitter.simple_upload(filename=photo_path)
+    media_id = media.media_id
+    try:
+        twitter.tweet_photo(text=message_text or "", media_id=media_id)
+        logging.warning("tweet sent")
+        await update.message.reply_text(text="Tweeted photo!")
+    except Exception as e:
+        logging.error("could not send tweet")
+        await update.message.reply_text(
+            text="Failed to tweet photo. Error: " + str(e),
+        )
+    finally:
+        os.remove(photo_path)
+
+
+async def message_handler_not_implemented(update: Update, context: CallbackContext):
+    if update.message is None:
+        return
+
+    logging.error("type of message not supported")
+    await update.message.reply_text(
+        text="This type of message is not yet supported in the context of this application",
+    )
+    return
